e922fdf7678b3c8b52583aa23bd7a8c593bcf721
Larry Finger <Larry.Finger@lwfinger.net>
2015-12-02 15:38:40 -0600

    rtl8723bu: Use endian macros from kernel
    This change converts from usage of CONFIG_LITTLE_ENDIAN to __LITTLE_ENDIAN.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/rtw_byteorder.h
+++ include/rtw_byteorder.h
@@ -20,19 +20,10 @@
 #ifndef _RTL871X_BYTEORDER_H_
 #define _RTL871X_BYTEORDER_H_
 
-
-#if defined (CONFIG_LITTLE_ENDIAN) && defined (CONFIG_BIG_ENDIAN)
-#error "Shall be CONFIG_LITTLE_ENDIAN or CONFIG_BIG_ENDIAN, but not both!\n"
-#endif
-
-#if defined (CONFIG_LITTLE_ENDIAN)
-#ifndef CONFIG_PLATFORM_MSTAR389
+#if defined (__LITTLE_ENDIAN)
 #  include <little_endian.h>
-#endif
-#elif defined (CONFIG_BIG_ENDIAN)
-#  include <big_endian.h>
 #else
-#  error "Must be LITTLE/BIG Endian Host"
+#  include <big_endian.h>
 #endif
 
 #endif /* _RTL871X_BYTEORDER_H_ */
