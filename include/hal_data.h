6acf2542cf55e8fbd27aace35191da94a25eff53
Larry Finger <Larry.Finger@lwfinger.net>
2016-02-12 12:30:28 -0600

    rtl8723bu: Fix build when CONFIG_USB_SUSPEND is defined
    On BeagleBoneBlack, and probably other ARM platforms, the kernel configuration
    defines CONFIG_USB_SUSPEND. Defining this variable causes autosuspend_enter
    to be undefined. Adding the appropriate include fixes the build error.
    
    Note: The resulting code has not been tested by me. Other problems may occur.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- include/hal_data.h
+++ include/hal_data.h
@@ -548,6 +548,30 @@ typedef struct hal_com_data HAL_DATA_TYPE, *PHAL_DATA_TYPE;
 
 #endif
 
+extern unsigned char	RTW_WPA_OUI[];
+extern unsigned char	WMM_OUI[];
+extern unsigned char	WPS_OUI[];
+extern unsigned char	P2P_OUI[];
+extern unsigned char	WFD_OUI[];
+
+int pm_netdev_open(struct net_device *pnetdev,u8 bnormal);
+void *scdb_findEntry(_adapter *priv, unsigned char *macAddr, unsigned char *ipAddr);
+void dhcp_flag_bcast(_adapter *priv, struct sk_buff *skb);
+int nat25_handle_frame(_adapter *priv, struct sk_buff *skb);
+int nat25_db_handle(_adapter *priv, struct sk_buff *skb, int method);
+void nat25_db_expire(_adapter *priv);
+extern unsigned char REALTEK_96B_IE[];
+int rtw_change_ifname(_adapter *padapter, const char *ifname);
+void indicate_wx_scan_complete_event(_adapter *padapter);
+u8 rtw_do_join(_adapter *padapter);
+int _netdev_open(struct net_device *pnetdev);
+int netdev_open (struct net_device *pnetdev);
+u8 key_2char2num(u8 hch, u8 lch);
+u8 key_2char2num(u8 hch, u8 lch);
+void macstr2num(u8 *dst, u8 *src);
+#ifdef CONFIG_AUTOSUSPEND
+void autosuspend_enter(_adapter* padapter);
+#endif
 
 #endif //__HAL_DATA_H__
 
