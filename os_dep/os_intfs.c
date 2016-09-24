c020d3a61078445291fd18a2bbf4e8b70c8218cd
Larry Finger <Larry.Finger@lwfinger.net>
2016-09-24 09:26:59 -0500

    rtl8723bu: Make proper fix for freeze on unload
    The fix is to move the call to rtw_wdev_unregister() after the call to
    unregister_netdev(). Thanks to  WeiqiChenCN for finding the correct fix.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- os_dep/os_intfs.c
+++ os_dep/os_intfs.c
@@ -829,18 +829,13 @@ void rtw_unregister_netdevs(struct dvobj_priv *dvobj)
 
 		pnetdev = padapter->pnetdev;
 
-#ifdef CONFIG_IOCTL_CFG80211
-/* The following statement causes an unload of the driver to hang up.
- * Removing it solves that issue; however, any unintended consequences
- * of removing it are unknown.
- */
-//		rtw_wdev_unregister(padapter->rtw_wdev);
-#endif
-
 		if((padapter->DriverState != DRIVER_DISAPPEAR) && pnetdev) {
 
 			unregister_netdev(pnetdev); //will call netdev_close()
 		}
+#ifdef CONFIG_IOCTL_CFG80211
+		rtw_wdev_unregister(padapter->rtw_wdev);
+#endif
 	}
 }
 
