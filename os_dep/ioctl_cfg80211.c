9f3e0c8e1178e3a084f00b8bc9bb07f5d5f8a5e7
Larry Finger <Larry.Finger@lwfinger.net>
2019-05-08 11:14:01 -0500

    rtl8723bu: Initialize station info
    The entire struct should be initialized.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- os_dep/ioctl_cfg80211.c
+++ os_dep/ioctl_cfg80211.c
@@ -3631,7 +3631,7 @@ void rtw_cfg80211_indicate_sta_assoc(_adapter *padapter, u8 *pmgmt_frame, uint f
 		else // WIFI_REASSOCREQ
 			ie_offset = _REASOCREQ_IE_OFFSET_;
 	
-		sinfo.pertid = NULL;
+		memset(&sinfo, 0, sizeof(sinfo));
 		sinfo.filled = STATION_INFO_ASSOC_REQ_IES;
 		sinfo.assoc_req_ies = pmgmt_frame + WLAN_HDR_A3_LEN + ie_offset;
 		sinfo.assoc_req_ies_len = frame_len - WLAN_HDR_A3_LEN - ie_offset;
