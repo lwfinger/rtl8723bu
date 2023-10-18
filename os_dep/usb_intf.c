babd106ea5717da57e744ef7900a7098d504cd65
Larry Finger <Larry.Finger@lwfinger.net>
2023-10-18 11:35:21 -0500

    rtl8723bu: Back out the MODULE_IMPORT_NS() needed for Android
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- os_dep/usb_intf.c
+++ os_dep/usb_intf.c
@@ -1411,7 +1411,10 @@ static void __exit rtw_drv_halt(void)
 	rtw_mstat_dump(RTW_DBGDUMP);
 }
 
+#if 0
+/* If you are building for Android, you will need to change the 0 in the line above to a 1 */
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
 
 module_init(rtw_drv_entry);
 module_exit(rtw_drv_halt);
